851a9a27e769fc86b2a7959656ea03de248877b1
Chris Walker <chris.jaguar@gmail.com>
2013-08-18 17:48:29 -0400

    Calls the GetMatchHistory Api and displays results to the console

--- DotaBird.Core/Steam/DotaWebApi.cs
+++ DotaBird.Core/Steam/DotaWebApi.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
+//using System.Web.Script.Serialization;
 
 namespace DotaBird.Core.Steam
 {
@@ -9,8 +11,11 @@ namespace DotaBird.Core.Steam
     {
         public string GetMatchHistory()
         {
-            // TODO
-            return "";
+            var client = new WebClient();
+
+            var json = client.DownloadString("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key=A41B14673A53F4C0A5281A6C47637C9E");
+
+            return json;
         }
     }
 }
