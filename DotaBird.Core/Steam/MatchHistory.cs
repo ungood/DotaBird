f3fbab78a773f8a9285883b9c7e88db614725019
Jason Walker <ungood@onetrue.name>
2013-08-18 20:30:16 -0700

    Added skeleton of actual DotaWebAPI interface.

---
+++ DotaBird.Core/Steam/MatchHistory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotaBird.Core.Steam
+{
+    /// <summary>
+    /// If you look carefully, you'll notice the JSON returned by GetMatchHistory is not, in fact, a MatchHistory object,
+    /// it is an object with a single property, "result", which is the MatchHistory data.
+    /// </summary>
+    public class MatchHistoryEnvelope
+    {
+        public MatchHistory Result { get; set; }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MatchHistory
+    {
+        public int NumResults { get; set; }
+        public int TotalResults { get; set; }
+        public int ResultsRemaining { get; set; }
+
+        public List<MatchSummary> Matches { get; set; }
+    }
+
+    public class MatchSummary
+    {
+        public long MatchId { get; set; }
+    }
+}
---
+++ DotaBird.Core/Steam/MatchHistoryRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotaBird.Core.Steam
+{
+    /// <summary>
+    /// Represents a request to search the match history for DOTA 2.
+    /// </summary>
+    public class MatchHistoryRequest
+    {
+        /// <summary>
+        /// If set, restricts the results to games in which the named player was in.
+        /// </summary>
+        public String PlayerName { get; set; }
+
+        /// <summary>
+        /// If set, restricts the results to games equal to or older than this match id.
+        /// </summary>
+        public long? StartAtMatchId { get; set; }
+
+        /// <summary>
+        /// If set, restricts the results to games after this date/time.
+        /// </summary>
+        public DateTime? MinDate { get set; }
+
+        /// <summary>
+        /// If set, restricts the results to games before this date/time.
+        /// </summary>
+        public DateTime? MaxDate { get; set; }
+    }
+}
